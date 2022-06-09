00edd32c18013e1fcd1f998ae448c0cf0c8258b8
Caleb Heuer <ch@raiguard.me>
2022-06-08 23:26:21 -0600

    Initial commit

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Extract version for later use
+grep 'Version:' changelog.txt | awk '{print $2}' | head -1 > ../version
+version=$(cat ../version)
+
+# Datestamp changelog and commit
+sed -i "3s/.*/Date: $(date -I)/" changelog.txt
+git add changelog.txt
+git commit -m "Prepare to release version $version"
+
+# Create tag with changelog
+changelog=$(awk "/Version: $version$/ { flag = 1 } /^--/ { flag = 0 } flag { gsub(/^  /, \"\"); print }" changelog.txt | tail -n +3)
+git tag v$version -a -m "$changelog"
+
+# Create archive and upload it
+git archive --format=zip --prefix=raitestmod/ HEAD -o raitestmod_$version.zip
+# fmm publish raitestmod_$version.zip
+
+# Increment version
+newversion=$(echo "$version" | awk 'BEGIN { FS = "."; OFS = "." } { print $1, $2, $3 + 1 }')
+sed -i "s/^  \"version\":.*\$/  \"version\": \"$newversion\",/" info.json
+echo -e "---------------------------------------------------------------------------------------------------\nVersion: $newversion\nDate: ????\n  Features:\n$(cat changelog.txt)" > changelog.txt
+
+git add changelog.txt info.json
+git commit -m "Move to version $newversion"
+
+git push --atomic main v$version
